1089057fa3148b6bd40f148c18de072877816a66
RomanLaypanov <rrrrraaammmmaaaazzz@gmail.com>
2025-06-13 11:42:17 +0300

    задача 5.2.13 вторая проверка

--- src/SymmetricDifference.java
+++ src/SymmetricDifference.java
@@ -3,10 +3,10 @@ import java.util.Set;
 
 public class SymmetricDifference {
     public static Set symmetricDifference(Set set1, Set set2) {
-        Set<Object> set = new HashSet<>(set1);
+        Set<?> set = new HashSet<>(set1);
         set.addAll(set2);
 
-        Set<Object> interSet = new HashSet<>(set1);
+        Set<?> interSet = new HashSet<>(set1);
         interSet.retainAll(set2);
 
         set.removeAll(interSet);
